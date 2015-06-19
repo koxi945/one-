3e54e7cc4c5b82a27616753bfed4a290d36ab3f9
jiang <mylampblog@163.com>
2015-06-19 12:55:16 +0800

    常规

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'Wknod8gk0jUUTTSz3QgbgwgVBmN9afA3'),
 
     'cipher' => 'AES-256-CBC',
 
--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'database'  => env('DB_DATABASE', 'mrblog'),
+            'username'  => env('DB_USERNAME', 'op'),
+            'password'  => env('DB_PASSWORD', 'opqqq111!!!'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => 'bk_',
