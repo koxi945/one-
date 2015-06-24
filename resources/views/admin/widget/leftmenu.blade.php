87f772e77f853a001ee41953a260b8efe147308c
jiang <mylampblog@163.com>
2015-06-24 23:10:27 +0800

    菜单在少于3个或3个的情况下会全部展开。

--- resources/views/admin/widget/leftmenu.blade.php
+++ resources/views/admin/widget/leftmenu.blade.php
@@ -3,20 +3,21 @@
         <?php
             $mcaName = \App\Services\Admin\MCAManager::MAC_BIND_NAME;
             $MCA = app()->make($mcaName);
+            $lessThenThree = count($menu) <= 3 ? true : false;
         ?>
         <?php foreach($menu as $key => $value): ?>
         <?php $son = App\Services\Admin\Tree::getSonKey(); ?>
             <?php if(isset($value[$son])): ?>
                 <?php $checkFirst = $MCA->matchFirstMenu($value['module'], $value['class'], $value['action']); ?>
                 <li>
-                    <a href="javascript:;" data-target=".dashboard-menu-<?php echo $key;?>" class="nav-header <?php if( ! $checkFirst) echo 'collapsed'; ?>" data-toggle="collapse">
+                    <a href="javascript:;" data-target=".dashboard-menu-<?php echo $key;?>" class="nav-header <?php if( ! $checkFirst and ! $lessThenThree) echo 'collapsed'; ?>" data-toggle="collapse">
                         <?php echo $value['name']; ?><i class="fa fa-collapse"></i>
                     </a>
                 </li>
                 <?php if(is_array($value[$son]) && !empty($value[$son])): ?>
                     <!-- sub menu -->
                     <li>
-                        <ul class="dashboard-menu-<?php echo $key;?> nav nav-list collapse <?php if($checkFirst) echo 'in'; ?>">
+                        <ul class="dashboard-menu-<?php echo $key;?> nav nav-list collapse <?php if($checkFirst or $lessThenThree) echo 'in'; ?>">
                             <?php foreach($value[$son] as $skey => $svalue): ?>
                                 <?php $checkSecond = $MCA->matchSecondMenu($svalue['module'], $svalue['class'], $svalue['action']); ?>
                                 <?php $urlParam = $svalue['module'] .'.'. $svalue['class'] .'.'. $svalue['action']; ?>
